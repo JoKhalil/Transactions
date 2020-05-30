4442176a0f6d2e30ea017aadbffce5907ea2f01c
zouz82 <jagal_zouzou@hotmail.com>
2020-05-30 15:33:56 -0400

    modification du modele compte pour tp5

--- Modele/Compte.php
+++ Modele/Compte.php
@@ -1,20 +1,27 @@
 <?php
 
-require_once 'Modele/Modele.php';
+//require_once 'Modele/Modele.php';
+require_once 'Framework/Modele.php';
 
 class Compte extends Modele {
     
     public function getComptes() {
-        $sql = 'SELECT * FROM compte ORDER BY ID_Compte DESC';
+        $sql = 'select c.ID_Compte, c.ID_Utilisateur, c.NomCompte, c.Balance,'
+                . 'c.MotDePasse, c.TypeDeCompte, c.EmailCompte, u.nom, u.identifiant'
+                . 'from comptes c inner join utilisateurs u on c.ID_Utilisateur = u.id'
+                . 'order by id desc';
         $comptes = $this->executerRequete($sql);
         return $comptes;
     }
     
     public function getCompte($idCompte) {
-        $sql = 'select * from compte'
-                . ' where ID_Compte=?';
+        $sql = 'select c.ID_Compte, c.ID_Utilisateur, c.NomCompte, c.Balance,'
+                . 'c.MotDePasse, c.TypeDeCompte, c.EmailCompte, u.nom'
+                . 'from comptes c inner join utilisateurs u on c.ID_Utilisateur = u.id'
+                . 'where c.ID_Compte=?';
+        
         $compte = $this->executerRequete($sql, array($idCompte));
-      
+        
         if ($compte->rowCount() == 1) {
             return $compte->fetch();  // Accès à la première ligne de résultat
         } else {
